7319a7a2fca5d4c9be9ee7ba7b2ca47088f2cbd3
Valerie Montoya <valeriemontoya90@gmail.com>
2015-11-15 19:30:01 +0100

    fix ControlBarItem layout

--- src/main/java/com/proj/gui/components/ControlBarItem.java
+++ src/main/java/com/proj/gui/components/ControlBarItem.java
@@ -4,26 +4,23 @@ import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
-public class ControlBarItem extends JPanel{
+public class ControlBarItem extends Box{
 	private JLabel nameLabel;
 	private JTextField valueTextField;
 	
 	public ControlBarItem(String name, String value) {
+		super(BoxLayout.Y_AXIS);
 		this.setOpaque(false);
 		this.nameLabel = new JLabel();
 		this.nameLabel.setForeground(new Color(255,255,250));
 		this.valueTextField = new JTextField();
 		this.valueTextField.setForeground(new Color(0,0,0));
 		
-		this.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx=0;
-		this.add(nameLabel,c);
-		this.add(valueTextField,c);
+
+		this.add(nameLabel);
+		this.add(valueTextField);
 		this.setName(name);
 		this.setValue(value);
 	}

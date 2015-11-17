e049aced7808348c0e1b88f0e6646d3bd9ba4ce2
Valerie Montoya <valeriemontoya90@gmail.com>
2015-11-17 21:39:28 +0100

    Add comments in simulator.java
    Add comments to better understand actions about thread ui (with mainwindow) and thread simulator

--- src/main/java/com/proj/core/Simulator.java
+++ src/main/java/com/proj/core/Simulator.java
@@ -38,6 +38,8 @@ public class Simulator extends Thread implements ActionListener {
 			}
 			if (!isLaunched) {
 				try {
+					// Ici c'est le thread simulator qui dépose un verrou si elle dispose
+					// (ou attend une ressource)
 					synchronized (this) {
 						this.wait();
 					}
@@ -61,24 +63,44 @@ public class Simulator extends Thread implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		// Executé dans le thread UI.
 		if(e.getSource() == this.view.getLaunchButton()){
 			onLaunchListener();
 		}
 	}
 	
 	public void onLaunchListener(){
+		// Executé dans le thread UI.
 		if (this.getState() == State.WAITING) { // if simulator is paused we relaunch it
+			
 			synchronized(this){
 				this.notify();
 				this.isLaunched = true;
 				this.view.getLaunchButton().setText("Pause");
 			}
-		} else if (this.getState() == State.NEW) { // if simulator is just created we start it
-			this.start();
+		} else if (this.getState() == State.NEW) {
+			// Lors du premier clic sur le boutton Launch
+			// Le thread de backdground (this) n'a pas encore été activé.
+			// Pas de risque d'accès concurrent. Pas besoin donc de synchronisation.
 			this.isLaunched = true;
 			this.view.getLaunchButton().setText("Pause");
-		} else { // or else we force off it
+			this.start();
+		} else {
+			// Lors d'un appui sur le boutton pause
+			// On souhaite placer le thread du simulateur dans l'état WAITING
+			// On doit donc appeler la fonction wait().
+			// Sauf que, au moment du clic sur le boutton pause, on n'a pas l'assurance
+			// que le thread du simulateur soit dans l'état RUNNABLE, état nécessaire pour passer
+			// à l'état pause. De plus, compte tenu des opérations effectuées dans la fonction run()
+			// l'état le plus probable du thread simulator est TIMED_WAITING.
+			// Ainsi, on délegue l'appel de la méthode wait() à la méthode run en lui signalant
+			// par un booléen que l'état doit être mis à WAITING. En effet, la méthode run() pourra 
+			// faire l'appel à wait() immédiatement après la sortie de l'état TIMED_WAITING.
 			synchronized(this){
+				// Ici, on a besoin d'une synchronisation, parce que le thread simulator
+				// est en cours de fonctionnement et pourrait potentiellement faire
+				// un accès concurrent aux variables membres auxquelles on accède ici.
+				//
 				this.isLaunched = false;
 				this.view.getLaunchButton().setText("Lancer");
 			}
